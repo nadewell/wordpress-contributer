550ab9442ddc03a34ad3a7b67d0a25e86f264748
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-07-19 20:23:56 +0530

    Initial Commit
    Initial Commit

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/*********
+ * 
+ * @package Wordpress_Contributer
+ * 
+ * @since 1.0.0
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
